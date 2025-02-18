026cc0ea6d54219c257c2251a6be9f5670193b14
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-18 03:12:03 +0000

    first attempt at actor filmography. need to troubleshoot

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -13,11 +13,13 @@ class ActorsController < ApplicationController
     # get the actor's character_list from the 'characters' table
     character_list = Character.where({ :actor_id => the_id }) # relation of all the characters the actor has 
     
-    actor_filmography = Array.new # array of hashes to hold all pertinent info for /actors/:id web page
+    @actor_filmography = Array.new # array of hashes to hold all pertinent info for /actors/:id web page
 
     character_list.each do |the_character|
       # get the movie row
       the_movie = Movie.where({ :id => the_character.movie_id })[0]
+      # get the movie id
+      movie_id = the_movie.id
       # get movie title
       movie_title = the_movie.title
       # get movie year
@@ -28,10 +30,10 @@ class ActorsController < ApplicationController
       character_name = the_character
       
       # push all of this information into a hash
-      filmography_hash = { :movie_title => movie_title, :movie_year => movie_year, :director => director_name, :character => character_name }
+      filmography_hash = { :movie_title => movie_title, :movie_year => movie_year, :director => director_name, :character => character_name, :movie_id => movie_id }
 
       # push all of this information into the actor_filmography array
-      actor_filmography.push(filmography_hash)
+      @actor_filmography.push(filmography_hash)
     end
 
 
