0f9ffdf279000777ba682f53f6a3f88c1934fa99
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-18 01:59:06 +0000

    director details page complete

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,8 +6,12 @@ class DirectorsController < ApplicationController
   def show
     the_id = params.fetch("the_id")
 
+    # get director info from 'directors' table
     matching_records = Director.where({ :id => the_id }) # returns a relation with all matching rows (should be just one)
     @the_director = matching_records[0] # pull the first (and only) row only in the relation into an array
+
+    # get director's filmography from `movies` table
+    @filmography = Movie.where({ :director_id => the_id})
 
     render({ :template => "director_templates/details" })
   end
