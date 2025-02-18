b11000ce5f8cf5adf995de99bda01cd396d6cc22
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-18 01:49:54 +0000

    director details page coming along

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,5 +2,13 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list" })
   end
-end
 
+  def show
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => the_id }) # returns a relation with all matching rows (should be just one)
+    @the_director = matching_records[0] # pull the first (and only) row only in the relation into an array
+
+    render({ :template => "director_templates/details" })
+  end
+end
