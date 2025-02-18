d70315d2081393c56f35220e3c2d903663bd93ea
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-18 02:46:43 +0000

    part of actor details page complete

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,15 @@ class ActorsController < ApplicationController
   def index
     render({ :template => "actor_templates/list" })
   end
+
+  def show
+    the_id = params.fetch("the_id")
+
+    # get actor info from 'actors' table
+    matching_records = Actor.where({ :id => the_id }) # pull relation with all matches (should be one)
+    @the_actor = matching_records[0] # pull the first (and only) row from the matching_record relations into an array
+    
+
+    render({ :template => "actor_templates/details" })
+  end
 end
